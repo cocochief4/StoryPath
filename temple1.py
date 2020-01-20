7d438a323b1c12c6602055dd11cab8c4906eb41b
gabriel-08 <57821142+gabriel-08@users.noreply.github.com>
2020-01-20 15:03:51 -0800

    Update temple1.py

--- temple1.py
+++ temple1.py
@@ -5,6 +5,10 @@ characterInventory = storyPath.characterInventory
 stick = storyPath.stick
 mName = storyPath.mName
 runOrFight = storyPath.runOrFight
+print_delay = storyPath.print_delay
+tired = storyPath.tired
+clear_screen = storyPath.clear_screen
+name = storyPath.name
 
 
 def live_with_monster():
@@ -12,13 +16,14 @@ def live_with_monster():
     global stick
     global runOrFight
     global mName
+    global name
     print("You decide to befriend him.")
     time.sleep(1)
     print("He befriends you!")
     time.sleep(1)
     mName = input("What do you name him?")
     time.sleep(1)
-    print(storyPath.name + " has befriended " + mName + "!")
+    print(name + " has befriended " + mName + "!")
     time.sleep(1)
     print("After a while he gives you a stick.")
     time.sleep(1)
@@ -31,35 +36,35 @@ def live_with_monster():
     storyPath.clear_screen()
     time.sleep(5)
     print("MAZE FOREST")
-    storyPath.print_delay("Here is you inventory.", 1)
+    print_delay("Here is your inventory.", 1)
     print(characterInventory)
     time.sleep(1)
-    storyPath.print_delay("As you walk through this forest, you realize just how big it is.", 1.5)
-    storyPath.print_delay("You hear sounds that sound like talking.", 1)
-    storyPath.print_delay("You " + mName + "hide behind a bush.", 1)
-    storyPath.print_delay("You look behind the bush, and see a horde of garblins.", 1.5)
-    storyPath.print_delay("As you walk through this forest, you realize just how big it is.", 1.5)
-    storyPath.print_delay("You hear sounds that sound like talking.", 1)
-    storyPath.print_delay("You and " + mName + " hide behind a bush.", 1)
-    storyPath.print_delay("You look behind the bush, and see a horde of garblins.", 1.5)
+    print_delay("As you walk through this forest, you realize just how big it is.", 1.5)
+    print_delay("You hear sounds that sound like talking.", 1)
+    print_delay("You " + mName + "hide behind a bush.", 1)
+    print_delay("You look behind the bush, and see a horde of garblins.", 1.5)
+    print_delay("As you walk through this forest, you realize just how big it is.", 1.5)
+    print_delay("You hear sounds that sound like talking.", 1)
+    print_delay("You and " + mName + " hide behind a bush.", 1)
+    print_delay("You look behind the bush, and see a horde of garblins.", 1.5)
     print("They are half gargoyle, half goblin creatures.")
     time.sleep(1)
-    storyPath.print_delay("And they're usually defending something...", 1.2)
-    storyPath.print_delay("Do you want to fight the garblins, or run away?", 1.2)
+    print_delay("And they're usually defending something...", 1.2)
+    print_delay("Do you want to fight the garblins, or run away?", 1.2)
     runOrFight = input("run (r) or fight (f)")
     if runOrFight.lower()[:1:] == "r":
         print("You and the monster barely outrun the garblins.")
-        storyPath.tired()
+        tired()
     else:
         print("You and the monster jump out of the bush and succesfully defend against the garblins.")
         print("You walk a bit further in the forrest, and come across a huge temple like structure.")
         time.sleep(5)
-        storyPath.clear_screen()
+        clear_screen()
         print("THE TEMPLE OF THE GARBLINS")
         print("Temple")
         time.sleep(1)
-        storyPath.print_delay("Here is your inventory.", 1)
-        storyPath.print_delay(characterInventory, 4)
+        print_delay("Here is your inventory.", 1)
+        print_delay(characterInventory, 4)
         print("As you walk through the temple, you see how old it is.")
         print("You find a first aid kit on the ground with a note on it.")
         print("The note reads,")
@@ -71,7 +76,7 @@ def live_with_monster():
         print("Do you want to take a different route or keep going on the same route?")
         different_route = input("take a different route(t) or keep going (k)")
         if different_route.lower()[:1:] == "t":
-            storyPath.print_delay("", 0)
+            print_delay("", 0)
         else:
             print("you keep hearing the sounds of garblins.")
         # Temporarily ending the function
