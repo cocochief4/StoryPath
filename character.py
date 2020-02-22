db02fc1b449e147c1f887ac7d911781fc6289866
gabriel-08 <57821142+gabriel-08@users.noreply.github.com>
2020-02-22 15:29:16 -0800

    Update character.py

--- character.py
+++ character.py
@@ -2,3 +2,17 @@ class Character:
     def __init__(self, name, inventory):
         self.name = name
         self.inventory = inventory
+        self.health = 100
+
+    @property
+    def in_health(self, increment):
+        return self.health + increment
+
+    @property
+    def de_health(self, decrement):
+        return self.health - decrement
+
+    @property
+    def add_inventory(self, item, amount):
+        amount += amount
+        return self.inventory.append(f"{amount} : {item}")
